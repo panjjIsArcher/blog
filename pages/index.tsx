035d08e38500c261009ec78f9b626b65c9c7888e
panjiajie <251907882@qq.com>
2023-11-24 15:58:10 +0800

    feat：文件结构

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 
+import "@/public/style/universal.scss"; // universal style
 
 export default function Home() {
   return (
-    <div>这是引导页</div>
+    <div>这是引导页
+      <div className="position-center" />
+    </div>
   )
 }
