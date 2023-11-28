be04dc45915bca5ac1cada37356156e376cfc4e1
panjiajie <251907882@qq.com>
2023-11-28 15:00:16 +0800

    new img

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import homeStyle from "@/public/style/home.module.scss"
 export default function Home() {
   return (
     <div className={`${homeStyle.bg} bg-universal`} >
-      <div >ddd  </div>
+      <h6 className={homeStyle["sub-title"]}>It is what we do that defines us  </h6>
+      <h1 className={homeStyle["main-title"]}>Dowson’s Blog</h1>
+      <span className={homeStyle.btn}>VIEW MORE</span>
     </div>
   )
 }
