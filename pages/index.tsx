2eb3ebf469159780ca2146d9151fa2e630fa694a
panjiajie <251907882@qq.com>
2023-11-28 14:12:55 +0800

    全局样式

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import universalStyle from "../public/style/universal.module.scss"; // universal style
-import deviceStyle from "../public/style/device.module.scss";
+
+import homeStyle from "@/public/style/home.module.scss"
 export default function Home() {
   return (
-    <div className={deviceStyle.div}>这是引导页
-      <div className={universalStyle["position-center"]} />
+    <div className={`${homeStyle.bg} bg-universal`} >
+      <div >ddd  </div>
     </div>
   )
 }
