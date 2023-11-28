bfe569651dca74dc1f2e703c3e3fdff50fcfd5d5
panjiajie <251907882@qq.com>
2023-11-28 18:49:38 +0800

    rem 调整为1rem=100px

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import "../public/style/device.scss"
-import "../public/style/universal.scss"
-
+import "../public/style/device.scss";
+import "../public/style/universal.scss";
+import { reCalc } from "@/public/utils/deviceAdapter";
+import { useEffect } from "react";
 // 新创建的 `pages/_app.js` 文件中必须有此默认的导出（export）函数
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    // 1rem = 100px
+    reCalc(375, 375);
+  }, [])
+  return <Component {...pageProps} />;
 }
