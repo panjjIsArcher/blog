c7d8d68360b888998a809390d78bf3914460411d
panjiajie <251907882@qq.com>
2023-11-29 13:56:33 +0800

    jump url

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,19 @@
 import { isBgImgLoaded } from "@/public/utils/load"
 import homeStyle from "@/public/style/home.module.scss"
 import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
 export default function Home() {
 
   const [isLoad, setIsLoad] = useState(false)
+  const [leaving, setLeaving] = useState(false)
+  const router = useRouter()
+  const startLeave = () => {
+    setLeaving(true)
+  }
+
+  const jump = () => {
+    router.push("/introduction")
+  }
 
   useEffect(() => {
     isBgImgLoaded("#home-container").then(isLoad => {
@@ -13,10 +23,14 @@ export default function Home() {
   }, [])
 
   return (
-    <div className={`${homeStyle.bg} bg-universal`} id="home-container">
+    <div className={`${homeStyle.bg} ${leaving ? homeStyle.leave : ""}  bg-universal`} id="home-container" onAnimationEnd={jump}>
       <h6 className={`${homeStyle["animation-universal"]} ${homeStyle["sub-title"]}  ${isLoad ? homeStyle["title-animation"] : ""}`}>It is what we do that defines us  </h6>
       <h1 className={`${homeStyle["animation-universal"]} ${homeStyle["main-title"]}  ${isLoad ? homeStyle["title-animation"] : ""}`}>Dowson’s Blog</h1>
-      <span className={`${homeStyle["animation-universal"]} ${homeStyle.btn} ${isLoad ? homeStyle["title-animation"] : ""}`}>VIEW MORE</span>
+      <span className={`${homeStyle["animation-universal"]} ${homeStyle.btn} ${isLoad ? homeStyle["title-animation"] : ""}`}
+        onClick={startLeave}
+      >
+        VIEW MORE
+      </span>
     </div>
   )
 }
