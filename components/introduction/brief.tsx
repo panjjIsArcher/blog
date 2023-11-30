644c2844938dae4f471ac7475f2348a0d9435cb6
panjiajie <251907882@qq.com>
2023-11-30 17:33:07 +0800

    eslint

--- components/introduction/brief.tsx
+++ components/introduction/brief.tsx
@@ -2,6 +2,6 @@ import briefStyle from "@/public/style/brief.module.scss";
 
 export default function Brief() {
     return <div className={`${briefStyle.brief}`}>
-        <div className={`${briefStyle.right} `}></div>
+        <div className={`${briefStyle.right} `} />
     </div>
 }
