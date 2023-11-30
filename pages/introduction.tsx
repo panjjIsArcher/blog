82273cc5e2e370d6b715c98e9e67f610fb65f254
panjiajie <251907882@qq.com>
2023-11-30 16:02:45 +0800

    scroll

--- pages/introduction.tsx
+++ pages/introduction.tsx
@@ -2,10 +2,23 @@ import introStyle from "@/public/style/introduction.module.scss";
 import { useEffect, useState } from "react";
 export default function Introduction() {
     const [startLoad, setStartLoad] = useState(false)
-    useEffect(() => {
+    const [startScroll, setStartScroll] = useState(false)
+
+    
+    useEffect( () => {
+        const fetch = async () =>{
+            await new Promise(resolve => setTimeout(resolve, 2.5 * 1000))
+            setStartScroll(true)
+        }
         setStartLoad(true)
+        fetch()
+        
     }, [])
-    return <div className={`${introStyle.container} `}>
-        <div className={`${startLoad ? introStyle.animation : ""} ${introStyle.content}`}></div>
+    return <div className={`${introStyle.container}  ${startScroll?introStyle["enable-scroll"]:""}`}>
+        <div className={`${startLoad ? introStyle.animation : ""} ${introStyle.content}`} >
+            <section className={introStyle.section}>板块1</section>
+            <section className={introStyle.section}>板块2</section>
+            <section className={introStyle.section}>板块3</section>
+        </div>
     </div>
 }
