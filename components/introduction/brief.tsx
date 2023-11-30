9699026ecb36bd4882ec4e815db2dcd17e57bf97
panjiajie <251907882@qq.com>
2023-11-30 18:09:43 +0800

    list

--- components/introduction/brief.tsx
+++ components/introduction/brief.tsx
@@ -1,7 +1,30 @@
 import briefStyle from "@/public/style/brief.module.scss";
 
+const list = [
+    {
+        name: "experience"
+    },
+    {
+        name: "skill stack"
+    },
+    {
+        name: "connect me"
+    }]
+
+
 export default function Brief() {
-    return <div className={`${briefStyle.brief}`}>
+    const ul = list.map((li, index, arr) => {
+        const length = arr.length;
+        const height = `${100 / length}%;`
+        return <li key={li.name} style={{height}}>{li.name}</li>
+    })
+
+    return <div className={briefStyle.brief}>
+        <div className={briefStyle.left} >
+            <ul className={briefStyle.menu} >
+                {ul}
+            </ul>
+        </div>
         <div className={`${briefStyle.right} `} />
     </div>
 }
