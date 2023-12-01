91f6dc067da141921c8237a1f50702749810428d
panjiajie <251907882@qq.com>
2023-12-01 18:38:18 +0800

    feat：新增事件传参

--- components/introduction/brief.tsx
+++ components/introduction/brief.tsx
@@ -18,17 +18,18 @@ const list = [
 
 export default function Brief() {
 
-    const [index,setIndex] =useState(0)
+    const [index, setIndex] = useState(0)
 
-    const changeIndex = (e) =>{
-        console.log(e.target)
-        setIndex(index)
+    const changeIndex = (e:any) => {
+        const dataset = e.target.dataset
+        setIndex(dataset.index)
+       
     }
 
     const ul = list.map((li, index, arr) => {
         const length = arr.length;
         const gap = 25 - 5 * index;
-        return (<li key={li.name} id={li.name} onMouseEnter={changeIndex} value={index}>
+        return (<li key={li.name} id={li.name} onMouseEnter={changeIndex} value={index} data-index={index}>
             <h5 className={briefStyle.title}> {li.name}</h5>
             <style jsx>{`h5{left:${gap}%}}`}</style>
             <style jsx>{`li{height:${100 / length}%;`} </style>
