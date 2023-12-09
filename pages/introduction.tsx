e66653e65b0a64aa34617d2d904f9aca93298422
panjiajie <251907882@qq.com>
2023-12-09 10:29:37 +0800

    3d title

--- pages/introduction.tsx
+++ pages/introduction.tsx
@@ -9,17 +9,25 @@ import src from "../public/audio/impossibleMission.mp3";
 export default function Introduction() {
     const [startLoad, setStartLoad] = useState(false)
     const [startScroll, setStartScroll] = useState(false)
+    let audioPLayer: HTMLAudioElement | null;
 
     const play = () => {
         const audio = new Audio(src)
         audio.play()
-
+        audioPLayer = audio
 
     }
     useEffect(() => {
         play()
         setStartLoad(true)
         setStartScroll(true)
+
+        return () => {
+            if (audioPLayer) {
+                audioPLayer.pause()
+                audioPLayer = null
+            }
+        }
     }, [])
 
     return <div className={`${introStyle.container}  ${startScroll ? introStyle["enable-scroll"] : ""}`}>
