96da22aaa25a9535bcaf29713af8e920817432f4
panjiajie <251907882@qq.com>
2023-12-11 15:15:56 +0800

    title

--- components/introduction/experience.tsx
+++ components/introduction/experience.tsx
@@ -1,6 +1,8 @@
 import { labels as labelsData } from "@/mock/database";
 import experienceStyle from "@/public/style/experience.module.scss";
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import avatar from "@/public/image/avatar.jpg"
 
 function Card() {
     const [labels, setLabels] = useState<string[]>([])
@@ -11,8 +13,17 @@ function Card() {
     }
 
     const labelsList = () => {
-        return labels.map(label => {
-            return <li key={label}>{label}</li>
+        return labels.map((label, index) => {
+            return <li key={label} className={`{experienceStyle.label} label-item`}>
+                {/* animation-delay: ${index * 0.3}s; */}
+                <style jsx>{
+                    `.label-item{
+                       --delay: ${index * 0.3}s;
+                    }`
+                }</style>
+                {label}
+
+            </li>
         })
     }
 
@@ -23,9 +34,10 @@ function Card() {
     })
 
     return <div className={`${experienceStyle["card-container"]} position-center`}>
+
         <div className={`${experienceStyle.back} ${experienceStyle.card} `} />
         <div className={`${experienceStyle.front} ${experienceStyle.card}`}>
-            <img src="" alt="" />
+            <Image src={avatar} layout="responsive" className={experienceStyle.image} />
             <ul>
                 {labelsList()}
             </ul>
@@ -48,6 +60,8 @@ export default function Experience() {
     return <div className={experienceStyle.container}>
         <div className={experienceStyle.left}>
             <nav className={`${experienceStyle.nav} iconfont   ${cardVisible ? "icon-zhankai" : "icon-close-bold"}`} onClick={changeNav} />
+            <h4 className={experienceStyle.title}>Bruce Dowson</h4>
+            <h5 className={experienceStyle.subtitle}>go by 「Jiajie Pan」</h5>
             {cardVisible ? <Card /> : <Ul />}
         </div>
 
