169be6889d4b756e9a42c03fa61c69e4feb534c0
panjiajie <251907882@qq.com>
2023-12-12 10:53:27 +0800

    mitt

--- components/introduction/projects.tsx
+++ components/introduction/projects.tsx
@@ -1,7 +1,26 @@
 import proStyle from "@/public/style/projects.module.scss";
+import { intersectionObserver } from "@/public/utils/deviceAdapter";
+import { useEffect, useRef, useState } from "react";
 
 export default function Projects() {
-    return <div className={proStyle.container}>
-        <div className={proStyle.line}></div>
-    </div>
+    const box = useRef(null)
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        intersectionObserver(box.current, (entries) => {
+            const isIntersecting: boolean = entries.every((entry: { isIntersecting: boolean }) => entry.isIntersecting)
+            if (isIntersecting && !loaded) {
+                setLoaded(isIntersecting)
+            }
+
+        }, { threshold: 0.8 })
+    })
+
+
+    return (<div ref={box} className={proStyle.container}>
+        {loaded ? <div className={proStyle.middle}>
+            <div className={`${proStyle["left-box"]}  ${proStyle.box}`} />
+            <div className={`${proStyle["right-box"]}  ${proStyle.box}`} />
+        </div> : ""}
+    </div>)
 }
