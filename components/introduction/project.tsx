608bf19010d9b9c086808d6fa53530b4f196387f
panjiajie <251907882@qq.com>
2023-12-12 16:14:43 +0800

    fit

--- components/introduction/project.tsx
+++ components/introduction/project.tsx
@@ -1,3 +1,10 @@
-export default function Project() {
-    return <div></div>
+import { Project as ProjectType } from "@/type/brief"
+import proStyle from "@/public/style/projects.module.scss";
+
+export default function Project(props: ProjectType) {
+    const { name, id } = props
+    return <div>
+        {name}
+
+    </div>
 }
