6b0eedaf67697bc9ac14ace1c2920658c2df6301
panjiajie <251907882@qq.com>
2023-12-14 13:34:49 +0800

    全局loading

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,43 @@ import "../public/style/device.scss";
 import "../public/style/universal.scss";
 import "../public/font/iconfont.css";
 import { reCalc } from "@/public/utils/deviceAdapter";
-import { useEffect } from "react";
-
-
-// import autofit from "autofit.js";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router"
+import Loading from "@/components/loading"
 
 // 新创建的 `pages/_app.js` 文件中必须有此默认的导出（export）函数
 export default function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false)
+  const router = useRouter()
+  const routeChangeStart = (url, opts) => {
+    console.log(url, opts)
+    setLoading(true)
+  }
+  const routeChangeComplete = () => {
+    setLoading(false)
+  }
+
   useEffect(() => {
+    router.events.on("routeChangeStart", routeChangeStart)
+    router.events.on("routeChangeComplete", routeChangeComplete)
     reCalc(1920, 1920);
+
+    return () => {
+      router.events.off("routeChangeStart", routeChangeStart)
+      router.events.off("routeChangeComplete", routeChangeComplete)
+    }
   }, [])
-  return <Component {...pageProps} />;
+
+  const style = {
+    position: "fixed", left: 0, top: 0, backgroundColor: "#000", height: "100%", width: "100%"
+  }
+
+  return <div >
+    <Component {...pageProps} />
+    {loading ? <div style={style}>
+      <Loading />
+    </div> : ""}
+
+  </div>
+
 }
